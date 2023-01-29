695f555b7c5a62dc5719256179590ce3e4c2e89b
shubham lathiiya <90539853+shubhhamlathiya@users.noreply.github.com>
2023-01-29 11:05:53 +0530

    3 commit

--- Customer.php
+++ Customer.php
@@ -1,8 +1,4 @@
 <!DOCTYPE html>
-<!--
-Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
--->
 <html>
     <head>
         <meta charset="UTF-8">
@@ -18,13 +14,85 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
         include './header.php';
         ?>
         <div id="layoutSidenav_content">
-            <div class="card mb-4">
-                <div class="card-header">
-                    <i class="fas fa-table me-1"></i>
-                    Customer
-                </div>
-            </div>
-        </div>
+            <main>
+                <div class="card mb-4">
+                    <div class="card-header">
+                        <i class="fas fa-table me-1"></i>
+                        Offer
+                    </div>
+                    <form method="post">
+                        <div class="card-body">
 
+                            <table class="table table-striped">
+                                <thead>
+                                    <tr>
+                                        <th scope="col"></th>
+                                        <th scope="col">Name</th>
+                                        <th scope="col">Email</th>
+                                        <th scope="col">DOB</th>
+                                        <th scope="col">DL</th>
+                                        <th scope="col">AN</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    $query = "SELECT * FROM customer";
+                                    $result = $conn->query($query);
+                                    $id = 1;
+                                    while ($row = mysqli_fetch_array($result)) {
+                                        $Name = $row['Name'];
+                                        $Email = $row['Email'];
+                                        $DOB = $row['DOB'];
+                                        $DL = $row['DL'];
+                                        $AN = $row['AN'];
+                                        ?>
+                                        <tr id='tr_<?= $id ?>'>
+                                            <td><input type='checkbox' name='delete[]' value='<?= $Email ?>' ></td>
+                                            <td><?= $Name ?></td>
+                                            <td><?= $Email ?></td>
+                                            <td><?= $DOB ?></td>
+                                            <td><?= $DL ?></td>
+                                            <td><?= $AN ?></td>
+                                        </tr>
+                                        <?php
+                                        $id++;
+                                    }
+                                    ?>
+                                </tbody>
+                            </table>
+                           <input type="submit" class="btn btn-primary" name="customer_delete" id="but_delete" value="Delete Customer">
+                            <?php
+                            if (isset($_POST['customer_delete'])) {
+
+                                if (isset($_POST['delete'])) {
+                                    foreach ($_POST['delete'] as $deleteid) {
+//                                    echo $deleteid;
+                                        $deleteUser = "DELETE from offer WHERE Email='$deleteid'";
+                                        $conn->query($deleteUser);
+                                    }
+                                    echo "<script>window.location.href='Customer.php'</script>";
+                                } else {
+                                    echo '<script>Checkboxseleted();</script>';
+                                }
+                            }
+                            ?>
+                        </div>
+                    </form>
+                </div>
+            </main>
+            <footer class="py-4 bg-light mt-auto">
+                <div class="container-fluid px-4">
+                    <div class="d-flex align-items-center justify-content-between small">
+                        <div class="text-muted">Copyright &copy; Your Website 2022</div>
+                        <div>
+                            <a href="#">Privacy Policy</a>
+                            &middot;
+                            <a href="#">Terms &amp; Conditions</a>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+ 
     </body>
 </html>
--- header.php
+++ header.php
@@ -15,7 +15,7 @@
         ?>
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <!-- Navbar Brand-->
-            <a class="navbar-brand ps-3" href="index.html">Quick Car Hire</a>
+            <a class="navbar-brand ps-3" href="Dashboard.php">Quick Car Hire</a>
             <!-- Sidebar Toggle-->
             <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
             <!-- Navbar Search-->
@@ -78,5 +78,12 @@
                     </div>
                 </nav>
             </div>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+            <script src="js/scripts.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+            <script src="assets/demo/chart-area-demo.js"></script>
+            <script src="assets/demo/chart-bar-demo.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
+            <script src="js/datatables-simple-demo.js"></script>
     </body>
 </html>
